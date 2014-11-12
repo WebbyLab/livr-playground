fa80f0fb0858e72e17fc7a5e9f50f1c77757fd5c
Alexey Zaslavsky <pterolex@yandex.ru>
2014-11-12 18:27:02 +0200

    Add examples

--- src/presets/index.js
+++ src/presets/index.js
@@ -8,12 +8,32 @@ module.exports = [
             data: require("raw!./registration-form/input.raw")
         }
     },
-
     {
         id: 'Validation of nested object',
         payload: {
             rules: require("raw!./nested-object/rules.raw"),
             data: require("raw!./nested-object/input.raw")
+        }
+    },
+    {
+        id: 'Validation of simple order list',
+        payload: {
+            rules: require("raw!./simple-list/rules.raw"),
+            data: require("raw!./simple-list/input.raw")
+        }
+    },
+    {
+        id: 'Validation of order list with products objects',
+        payload: {
+            rules: require("raw!./list-of-objects/rules.raw"),
+            data: require("raw!./list-of-objects/input.raw")
+        }
+    },
+    {
+        id: 'Validation of order list with different product objects',
+        payload: {
+            rules: require("raw!./different-objects/rules.raw"),
+            data: require("raw!./different-objects/input.raw")
         }
     }
 ];
